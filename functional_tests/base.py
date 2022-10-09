d53d88fcb68fc8aba8d9ec2c85d08f12f7742e3e
YugandharG <yugandharg@ftcsolar.com>
2022-10-09 16:52:38 +0530

    Refactor functional tests

---
+++ functional_tests/base.py
@@ -0,0 +1,31 @@
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox(
+            executable_path=r'C:\Users\YugandharG\REPO\test-driven-development\geckodriver.exe')  # ADD geckodriver path here before testing
+
+    def tearDown(self) -> None:
+        self.browser.close()
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if (time.time() - start_time) > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
